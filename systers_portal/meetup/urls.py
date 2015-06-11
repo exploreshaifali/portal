1dddedf580bdf94f9c3d8f119942c313e9106438
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2015-06-12 02:09:18 +0530

    urls for meetup locations added.

---
+++ systers_portal/meetup/urls.py
@@ -0,0 +1,25 @@
+from django.conf.urls import url
+
+from meetup.views import MeetupLocationDetailView
+
+
+urlpatterns = [
+    url(r'^(?P<slug>\w+)/upcoming/$',
+        MeetupLocationDetailView.as_view(template_name='meetup/upcoming.html'),
+        name='upcoming_meetup_location'),
+    url(r'^(?P<slug>\w+)/past/$',
+        MeetupLocationDetailView.as_view(template_name='meetup/past.html'),
+        name='past_meetup_location'),
+    url(r'^(?P<slug>\w+)/about/$',
+        MeetupLocationDetailView.as_view(template_name='meetup/about.html'),
+        name='about_meetup_location'),
+    url(r'^(?P<slug>\w+)/organizers/$',
+        MeetupLocationDetailView.as_view(template_name='meetup/organizers.html'),
+        name='organizers_meetup_location'),
+    url(r'^(?P<slug>\w+)/members/$',
+        MeetupLocationDetailView.as_view(template_name='meetup/all_members.html'),
+        name='all_members_meetup_location'),
+    url(r'^(?P<slug>\w+)/sponsors/$',
+        MeetupLocationDetailView.as_view(template_name='meetup/sponsors.html'),
+        name='sponsors_meetup_location'),
+]
