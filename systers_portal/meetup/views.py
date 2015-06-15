df1439c0926a9596adb787b99271d32b41e4a0c9
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2015-06-15 20:45:53 +0530

    List view for meetup location

--- systers_portal/meetup/views.py
+++ systers_portal/meetup/views.py
@@ -1,8 +1,14 @@
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
 from meetup.models import MeetupLocation
 
 
 class MeetupLocationDetailView(DetailView):
     """Meetup Location page view, show only details that user requested"""
+    model = MeetupLocation
+
+
+class MeetupLocationListView(ListView):
+    """ List all Meetup Locations"""
     model = MeetupLocation
